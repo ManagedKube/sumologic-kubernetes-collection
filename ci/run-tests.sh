e2c3e2d02c9bfb93f8550c05c22486c9629db966
Dominik Rosiek <drosiek@sumologic.com>
2020-10-02 17:25:25 +0200

    Refactor run_tests.sh

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
+set -e
 
 VERSION="${TRAVIS_TAG:-0.0.0}"
 VERSION="${VERSION#v}"
+
+function test_fluentd_plugin() {
+  local plugin_name="${1}"
+  local version="${2}"
+  # Strip everything after "-" (longest match) to avoid gem prerelease behavior
+  local gem_version="${version%%-*}"
+
+  cd "${plugin_name}" || exit 1
+
+  if [[ "${version}" == "" ]] ; then
+    echo "Please provide the version when bundling fluentd plugins"
+    exit 1
+  fi
+
+  echo "Preparing gem ${plugin_name} version ${gem_version} in $(pwd) for testing ..."
+  sed -i.bak "s/0.0.0/${gem_version}/g" ./"${plugin_name}".gemspec
+  rm -f ./"${plugin_name}".gemspec.bak
+
+  echo "Install bundler..."
+  bundle install
+
+  echo "Run unit tests..."
+  bundle exec rake || exit 1
+}
 
 function test_fluentd_plugins() {
   local version="${1}"
@@ -11,52 +36,27 @@ function test_fluentd_plugins() {
     exit 1
   fi
 
-  for i in ./fluent-plugin-*/ ; do
-    if [[ -d "${i}" ]]; then
-    (
-      cd "${i}" || exit 1
-      local plugin_name
-      plugin_name="$(basename "${i}")"
-      # Strip everything after "-" (longest match) to avoid gem prerelease behavior
-      local gem_version="${version%%-*}"
-      echo "Preparing gem ${plugin_name} version ${gem_version} in $(pwd) for testing ..."
-      sed -i.bak "s/0.0.0/${gem_version}/g" ./"${plugin_name}".gemspec
-      rm -f ./"${plugin_name}".gemspec.bak
-
-      echo "Install bundler..."
-      bundle install
-
-      echo "Run unit tests..."
-      bundle exec rake
-    )
-    fi
-  done
+  find . -name 'fluent-plugin-*' -type 'd' -print |
+    while read -r line; do
+      # Run tests in their own context
+      (test_fluentd_plugin "$(basename "${line}")" "${version}") || exit 1
+    done
 }
 
-## check the build script with shellcheck
-## TODO: the "|| true" prevents the build from failing on shellcheck errors - to be removed
+## check the build scripts with shellcheck
 echo "Checking the build script with shellcheck..."
-shellcheck ci/build.sh || true
-shellcheck ci/run-tests.sh || true
+shellcheck ci/build.sh
+shellcheck ci/run-tests.sh
 
 # Test if template files are generated correctly for various values.yaml
 echo "Test helm templates generation"
-if ./tests/run.sh; then
-  echo "Helm templates generation test passed"
-else
-  echo "Tracing templates generation test failed"
-  exit 1
-fi
+./tests/run.sh || (echo "Failed testing templates" && exit 1)
 
 # Test upgrade script
 echo "Test upgrade script..."
-if ./tests/upgrade_script/run.sh; then
-  echo "Upgrade Script test passed"
-else
-  echo "Upgrade Script test failed"
-  exit 1
-fi
+./tests/upgrade_script/run.sh || (echo "Failed testing upgrade script" && exit 1)
 
+# Test fluentd plugins
 test_fluentd_plugins "${VERSION}" || (echo "Failed testing fluentd plugins" && exit 1)
 
 echo "DONE"
