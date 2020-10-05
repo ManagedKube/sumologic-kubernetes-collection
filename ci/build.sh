89ec844813d754e8cb2d4da9d9c144613d0530fa
Dominik Rosiek <drosiek@sumologic.com>
2020-10-05 10:16:34 +0200

    Extract validate_changes_to_generated_files function

--- ci/build.sh
+++ ci/build.sh
@@ -101,6 +101,22 @@ function push_helm_chart() {
   set +x
 }
 
+function validate_changes_to_generated_files() {
+  local changes
+  local recent_author
+
+  # Check most recent commit author. If non-Travis, check for changes made to generated files
+  recent_author=$(git log origin-repo/master..HEAD --format="%an" | grep -m1 "")
+  if echo "$recent_author" | grep -v -q -i "travis"; then
+    # NOTE(ryan, 2019-08-30): Append "|| true" to command to ignore non-zero exit code
+    changes=$(git log origin-repo/master..HEAD --name-only --format="" --author="$recent_author" | grep -i "fluentd-sumologic.yaml.tmpl\|fluent-bit-overrides.yaml\|prometheus-overrides.yaml\|falco-overrides.yaml") || true
+    if [ -n "$changes" ]; then
+      echo "Aborting due to manual changes detected in the following generated files: $changes"
+      exit 1
+    fi
+  fi
+}
+
 echo "Running tests"
 ./tests/run_tests || (echo "Failed running tests" && exit 1)
 
@@ -112,16 +128,7 @@ fi
 # Check for invalid changes to generated yaml files (non-Tag builds)
 # Exclude branches that start with "revert-" to allow reverts
 if [ -n "$GITHUB_TOKEN" ] && [ "$TRAVIS_EVENT_TYPE" == "pull_request" ] && [[ ! "$TRAVIS_PULL_REQUEST_BRANCH" =~ ^revert- ]]; then
-  # Check most recent commit author. If non-Travis, check for changes made to generated files
-  recent_author=$(git log origin-repo/master..HEAD --format="%an" | grep -m1 "")
-  if echo "$recent_author" | grep -v -q -i "travis"; then
-    # NOTE(ryan, 2019-08-30): Append "|| true" to command to ignore non-zero exit code
-    changes=$(git log origin-repo/master..HEAD --name-only --format="" --author="$recent_author" | grep -i "fluentd-sumologic.yaml.tmpl\|fluent-bit-overrides.yaml\|prometheus-overrides.yaml\|falco-overrides.yaml") || true
-    if [ -n "$changes" ]; then
-      echo "Aborting due to manual changes detected in the following generated files: $changes"
-      exit 1
-    fi
-  fi
+  validate_changes_to_generated_files || (echo "Aborting due to manual changes detected in generated files" && exit 1)
 fi
 
 bundle_fluentd_plugins "${VERSION}" || (echo "Failed bundling fluentd plugins" && exit 1)
